97246376402dd91f7b82a121fec66961979062df
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-05-23 17:28:36 -0400

    Add addSpell method

--- src/Magic/Magician.java
+++ src/Magic/Magician.java
@@ -5,11 +5,16 @@ import java.util.ArrayList;
 public class Magician {
     private String name;
 
-    private ArrayList<Spell> magicianSpells;
+    private ArrayList<Spell> magicianSpells = new ArrayList<>();
 
     public Magician(String name)
     {
         this.name = name;
+    }
+
+    public void addSpell(Spell newSpell)
+    {
+        magicianSpells.add(newSpell);
     }
 
 
