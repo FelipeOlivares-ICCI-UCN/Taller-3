3725ff291eb91991d3f6d862c6a0a53ace7d4617
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-05-31 16:06:08 -0400

    Create SpellFactory.java

---
+++ src/DataManager/SpellFactory.java
@@ -0,0 +1,17 @@
+package DataManager;
+
+import Magic.Spells.*;
+
+public class SpellFactory {
+
+    public static Spell getSpell(String[] data)
+    {
+        if ("fuego".equalsIgnoreCase(data[0])) return new FireSpell(data);
+        else if ("tierra".equalsIgnoreCase(data[0])) return new EarthSpell(data);
+        else if ("agua".equalsIgnoreCase(data[0])) return new WaterSpell(data);
+        else if ("planta".equalsIgnoreCase(data[0])) return new PlantSpell(data);
+        else throw new IllegalArgumentException("Unknown Spell type: " + data[0]);
+    }
+
+
+}
