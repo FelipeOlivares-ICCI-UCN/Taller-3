20b43d4241f513aeaff4b9ecf27e22be5c487055
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-05-31 16:06:45 -0400

    Delete spellParser method

--- src/DataManager/SpellReader.java
+++ src/DataManager/SpellReader.java
@@ -35,45 +35,6 @@ public class SpellReader {
     }
 
 
-    private Spell spellParser(String data)
-    {
-        String[] spellData = data.split(";");
-        String spellName = spellData[0];
-        String spellType = spellData[1];
-        int spellDamage = Integer.parseInt(spellData[2]);
-
-        if (spellType.equals("Fuego"))
-        {
-            int burnTime = Integer.parseInt(spellData[3]);
-            return new FireSpell(spellName, spellType, spellDamage, burnTime);
-        }
-
-        else if (spellType.equals("Tierra"))
-        {
-            int defenseUpgrade = Integer.parseInt(spellData[3]);
-            return new EarthSpell(spellName, spellType, spellDamage, defenseUpgrade);
-        }
-
-        else if (spellType.equals("Planta"))
-        {
-            int stunDuration = Integer.parseInt(spellData[3].split(",")[0]);
-            int quantPlant = Integer.parseInt(spellData[3].split(",")[1]);
-
-            return new PlantSpell(spellName, spellType, spellDamage, stunDuration, quantPlant);
-        }
-
-        else if (spellType.equals("Agua"))
-        {
-            int quantHeal = Integer.parseInt(spellData[3].split(",")[0]);
-            int waterPressure = Integer.parseInt(spellData[3].split(",")[1]);
-
-            return new WaterSpell(spellName, spellType, spellDamage, quantHeal, waterPressure);
-        }
-
-        //Only used to return something if everything else fails.
-        return new Spell(spellName, spellType, spellDamage);
-
-    }
 
 
 
