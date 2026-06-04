09819e32d0eacf4eb4c906358c613ae818cfdb83
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-06-03 20:27:16 -0400

    Create AppSystemImpl

---
+++ src/Logic/AppSystemImpl.java
@@ -0,0 +1,35 @@
+package Logic;
+
+import Magic.Factory.*;
+import Magic.Magician.*;
+import Magic.Spells.Spell;
+
+import java.util.ArrayList;
+
+public class AppSystemImpl implements AppSystem {
+
+    public static ArrayList<Spell> globalSpells = new ArrayList<>();
+
+    @Override
+    public void startMenu()
+    {
+        for (Spell spell: globalSpells)
+        {
+            System.out.println(spell.getName());
+
+        }
+    }
+
+    @Override
+    public void loadSpellData(String[] data)
+    {
+        globalSpells.add(SpellFactory.getSpell(data));
+    }
+
+    @Override
+    public void loadMageData(String[] data) {
+
+    }
+
+
+}
