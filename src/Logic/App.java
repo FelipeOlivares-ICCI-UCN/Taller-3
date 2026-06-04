0fb9fa77882ab0272e1526235dc606dc0cf1f7a5
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-06-03 20:27:31 -0400

    Add Entry point

--- src/Logic/App.java
+++ src/Logic/App.java
@@ -4,14 +4,39 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+
+
 public class App
 {
+    private static AppSystem s = new AppSystemImpl();
+
+    static void main(String[] args) throws FileNotFoundException
+    {
+        try
+        {
+            readSpellFile();
+        }
+
+        catch (Exception e)
+        {
+            System.out.println("Error while reading the file: " + e.getMessage());
+            return;
+        }
+
+        s.startMenu();
+
+
+
+    }
+
     private static void readSpellFile() throws FileNotFoundException
     {
         File arch = new File("./data/Hechizos.txt");
         Scanner lector = new Scanner(arch);
         while (lector.hasNextLine()) {
             String[] data = lector.nextLine().split(";");
+            s.loadSpellData(data);
+
         }
         lector.close();
     }
