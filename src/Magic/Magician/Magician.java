323e43590e73a0bbb277056c0e99c4acaa912158
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-06-03 20:26:40 -0400

    Temp changed Constructor and properties

--- src/Magic/Magician/Magician.java
+++ src/Magic/Magician/Magician.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 public class Magician {
     private String name;
 
-    private ArrayList<Spell> magicianSpells;
-
-    public Magician(String name, ArrayList<Spell> spells)
+    public Magician(String[] data)
     {
-        this.name = name;
-        this.magicianSpells = spells;
+        this.name = data[0];
+
+
     }
+
+
 
 
 }
