c785ec05b4541f8a8b077bacba13dda71ab1028e
Felipe Olivares <felipe.olivares04@alumnos.ucn.cl>
2026-06-03 20:33:20 -0400

    Fix wrong data index

--- src/Magic/Factory/SpellFactory.java
+++ src/Magic/Factory/SpellFactory.java
@@ -6,11 +6,11 @@ public class SpellFactory {
 
     public static Spell getSpell(String[] data)
     {
-        if ("fuego".equalsIgnoreCase(data[0])) return new FireSpell(data);
-        else if ("tierra".equalsIgnoreCase(data[0])) return new EarthSpell(data);
-        else if ("agua".equalsIgnoreCase(data[0])) return new WaterSpell(data);
-        else if ("planta".equalsIgnoreCase(data[0])) return new PlantSpell(data);
-        else throw new IllegalArgumentException("Unknown Spell type: " + data[0]);
+        if ("fuego".equalsIgnoreCase(data[1])) return new FireSpell(data);
+        else if ("tierra".equalsIgnoreCase(data[1])) return new EarthSpell(data);
+        else if ("agua".equalsIgnoreCase(data[1])) return new WaterSpell(data);
+        else if ("planta".equalsIgnoreCase(data[1])) return new PlantSpell(data);
+        else throw new IllegalArgumentException("Unknown Spell type: " + data[1]);
     }
 
 
